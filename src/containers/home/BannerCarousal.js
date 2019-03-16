f00c20a903beded5f7f7ccd16ec858a2c3acf5ad
shan2new <shantanusinha95@gmail.com>
2019-03-16 21:45:17 +0530

    Fix: Carousal Issue

--- src/containers/home/BannerCarousal.js
+++ src/containers/home/BannerCarousal.js
@@ -6,14 +6,18 @@ import ThirdSlide from '../../Images/ThirdSlide.jpg'
 
 const BannerCarousal = props => (
   <div>
-    <Carousel fade="true">
+    <Carousel fade={true} controls={false}>
       <Carousel.Item>
         <Carousel.Caption>
           <h3> Awake, Arise and Educate.</h3>
           <h2>Smash traditions, liberate. </h2>
           <p>Savitri Bai Phule</p>
         </Carousel.Caption>
-        <img className="d-block w-100" src={FirstSlide} alt="First slide" />
+        <img
+          className="d-block w-100"
+          src="holder.js/1200x700?theme=sky"
+          alt="First slide"
+        />
       </Carousel.Item>
       <Carousel.Item>
         <Carousel.Caption>
@@ -21,15 +25,23 @@ const BannerCarousal = props => (
           <h2>Smash traditions, liberate. </h2>
           <p>Savitri Bai Phule</p>
         </Carousel.Caption>
+        <img
+          className="d-block w-100"
+          src="holder.js/1200x700?theme=vine"
+          alt="Third slide"
+        />
       </Carousel.Item>
-      <img className="d-block w-100" src={SecondSlide} alt="Third slide" />
       <Carousel.Item>
         <Carousel.Caption>
           <h3> Awake, Arise and Educate.</h3>
           <h2>Smash traditions, liberate. </h2>
           <p>Savitri Bai Phule</p>
         </Carousel.Caption>
-        <img className="d-block w-100" src={ThirdSlide} alt="Third slide" />
+        <img
+          className="d-block w-100"
+          src="holder.js/1200x700?theme=social"
+          alt="Third slide"
+        />
       </Carousel.Item>
     </Carousel>
   </div>
