4535d8badb0e0423522e7644638ad16bc99b67c9
shan2new <shantanusinha95@gmail.com>
2019-04-27 15:39:47 +0530

    Work Image

--- src/containers/work/Team.js
+++ src/containers/work/Team.js
@@ -5,13 +5,19 @@ import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import CardDeck from 'react-bootstrap/CardDeck'
 import WorkPagePic from '../../Images/Display/workPagePic.jpg'
+import WorkPagePicMobile from '../../Images/Display/WorkImage_Mobile.png'
 
 const Team = () => (
   <div className="mt-5">
     <Container>
       <Row>
-        <Col style={{ paddingLeft: '100px', paddingRight: '100px' }}>
+        <Col
+          style={{ paddingLeft: '100px', paddingRight: '100px' }}
+          className="d-none d-md-block">
           <img src={WorkPagePic} />
+        </Col>
+        <Col className="d-block d-xs-block d-sm-block">
+          <img src={WorkPagePicMobile} style={{ maxWidth: '100%' }} />
         </Col>
       </Row>
     </Container>
